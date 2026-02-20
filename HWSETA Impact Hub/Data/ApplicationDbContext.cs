71aa3ec9a3f887f28e442d77070e913eb6dbb4bc
SibekoDev <conjugationjabulani@gmail.com>
2026-02-20 17:30:02 +0200

    Add project files.

---
+++ HWSETA Impact Hub/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HWSETA_Impact_Hub.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ HWSETA Impact Hub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HWSETA_Impact_Hub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
